4a987a2c881d5cb6d57e03e112cce48d571e24de
leal.philip <leal.philip@heb.com>
2024-09-26 20:56:48 -0500

    Adding intake shooter motors

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "2025_KOP";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 7;
+  public static final String GIT_SHA = "eeea2bb5a4a7004ca7e6286d9c16a5f7dfbbbc7a";
+  public static final String GIT_DATE = "2024-09-13 12:09:34 EDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2024-09-14 17:52:58 EDT";
+  public static final long BUILD_UNIX_TIME = 1726350778503L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants() {}
+}
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,27 +4,33 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
 import edu.wpi.first.util.sendable.SendableRegistry;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import org.littletonrobotics.junction.LoggedRobot;
 
 /**
  * This is a demo program showing the use of the DifferentialDrive class, specifically it contains
  * the code necessary to operate a robot with tank drive.
  */
-public class Robot extends TimedRobot {
+public class Robot extends LoggedRobot {
+  // public class Robot extends LoggedRobot {
   private DifferentialDrive m_robotDrive;
-  private Joystick m_leftStick;
-  private Joystick m_rightStick;
   XboxController controller;
 
   private final WPI_VictorSPX m_leftMotor1 = new WPI_VictorSPX(1);
   private final WPI_VictorSPX m_leftMotor2 = new WPI_VictorSPX(2);
   private final WPI_VictorSPX m_rightMotor3 = new WPI_VictorSPX(3);
   private final WPI_VictorSPX m_rightMotor4 = new WPI_VictorSPX(4);
+  
+  private final CANSparkMax motor1 = new CANSparkMax(5, MotorType.kBrushless);
+  private final CANSparkMax motor2 = new CANSparkMax(6, MotorType.kBrushless);
+  private final CANSparkMax motor3 = new CANSparkMax(7, MotorType.kBrushless);
 
   @Override
   public void robotInit() {
@@ -37,8 +43,6 @@ public class Robot extends TimedRobot {
     // result in both sides moving forward. Depending on how your robot's
     // gearbox is constructed, you might have to invert the left side instead.
     m_rightMotor3.setInverted(false);
-    //m_rightMotor4.setInverted(true);
-    
 
     m_rightMotor4.follow(m_rightMotor3);
     m_leftMotor2.follow(m_leftMotor1);
@@ -47,13 +51,26 @@ public class Robot extends TimedRobot {
 
     controller = new XboxController(0);
 
-    //m_leftStick = new Joystick(0);
-    //m_rightStick = new Joystick(1);
+    motor1.setIdleMode(IdleMode.kCoast);
+    motor2.setIdleMode(IdleMode.kCoast);
+    motor3.setIdleMode(IdleMode.kCoast);
+
+    motor2.follow(motor1);
+    motor3.follow(motor1);
+
+    // Start AdvantageKit logger
+    // Logger.start();
   }
 
   @Override
   public void teleopPeriodic() {
-    //m_robotDrive.tankDrive(-m_leftStick.getY(), -m_rightStick.getY());
     m_robotDrive.tankDrive(-controller.getLeftY(), -controller.getRightY());
+
+    if(controller.getRightBumperPressed()) {
+      motor1.setVoltage(2.0);
+      //motor1.set(2.0);
+    } else if(controller.getRightBumperReleased()) {
+      motor1.stopMotor();
+    }
   }
 }
